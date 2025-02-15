9511c4053abef44d70743099e1f1e0bb961ca0db
Manqoba Nkosi <manqobankosi007@gmail.com>
2025-02-15 22:32:04 +0200

    updated app.js

--- public/js/app.js
+++ public/js/app.js
@@ -146,7 +146,7 @@ document.getElementById('assignButton').addEventListener('click', function() {  
         // scannedItems[index].status = statusInput.value;
     // Update the scannedItems array
     scannedItems[index] = {
-        tagNumber: tagNumberInput.textContent.split(':'),
+        tagNumber: tagNumberInput.textContent.split(': '),
         category: categoryInput.value,
         subcategory: subcategoryInput.value,
         description: descriptionInput.value,
